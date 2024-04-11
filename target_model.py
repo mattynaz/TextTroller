83233c7e29535053f16ea5aa68c378bb323d7080
Matthew Nazari <matthewanazari@gmail.com>
2024-04-11 17:40:22 -0400

    set `max_new_tokens` on target model

--- target_model.py
+++ target_model.py
@@ -11,7 +11,7 @@ def generate(prompts: list[str]) -> list[str]:
     prompts = [f"[INST]{p}[/INST]" for p in prompts]
     inputs = tokenizer(prompts, return_tensors="pt", padding=True).to(device)
     with torch.no_grad():
-        outputs = model.generate(**inputs, max_length=100)
+        outputs = model.generate(**inputs, max_new_tokens=128)
 
     responses = tokenizer.batch_decode(outputs)
     responses = [r.split("[/INST]", 1)[-1].strip() for r in responses]
